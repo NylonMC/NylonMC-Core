d54cd8b32ee45fe74166c480369f893cbcdc5572
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-08-24 13:37:29 -0500

    git submodules. works in production now

---
+++ update_rubicon.sh
@@ -0,0 +1,10 @@
+git submodule update --init --recursive
+rm ./src/main/java/org/beeware/rubicon/Python.java
+mkdir -p ./src/main/java/org/beeware/rubicon/
+cp ./rubicon-java/org/beeware/rubicon/Python.java ./src/main/java/org/beeware/rubicon/Python.java
+rm ./src/main/java/org/beeware/rubicon/PythonInstance.java
+mkdir -p ./src/main/java/org/beeware/rubicon/
+cp ./rubicon-java/org/beeware/rubicon/PythonInstance.java ./src/main/java/org/beeware/rubicon/PythonInstance.java
+rm -r ./src/main/resources/python/rubicon
+mkdir -p ./src/main/resources/python/
+cp -r ./rubicon-java/rubicon ./src/main/resources/python/rubicon
